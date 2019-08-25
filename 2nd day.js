f79e4a317fe52fbfb8775bbe4058277b4ded4fd4
Bu-Omar <ahmed.abuhamad@gmail.com>
2019-08-26 01:20:22 +0300

    Update and rename 1st day to 2nd day.js

---
+++ 2nd day.js
@@ -0,0 +1,14 @@
+//comment 
+//trying JS...
+document.write("مرحباً بكم في عالم البرمجة");
+document.write("\n\r");
+document.write(5+7);
+document.write(5%5);
+
+var s = 5;
+document.write(s)
+
+document.write("/n/r");
+
+var x = 10;
+document.write("Hello, this is my first challenge and I will do it "+x+" times. I will do it, I will learn JavaScript.");
